9f4e443b24437cc608c20c3eb7eb292a623ba9c2
limerix932 <joliopguy@gmail.com>
2025-02-14 23:16:48 +1000

    init app

--- src/components/layout/AppLayout.jsx
+++ src/components/layout/AppLayout.jsx
@@ -1,5 +1,5 @@
 import { Layout, Spin } from "antd";
-import AppHeader from "./appHeader";
+import AppHeader from "./AppHeader";
 import AppSider from "./AppSider";
 import AppContent from "./AppContent";
 import { useContext } from "react";
